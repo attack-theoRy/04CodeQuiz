b3cc1ed5ca8a86aa3969a63406a87abad4f4d996
Pirooz Wallace <PiroozWallace@outlook.com>
2020-10-19 17:48:26 -0700

    question array
    starting with an array for the quiz questions

---
+++ script.js
@@ -0,0 +1,28 @@
+
+questionList = [ 
+    {
+        num: 1,
+        question: "What are the three major components of website building",
+        answer: "HTML, CSS. Javascript",
+        options: [
+            "PHP, Java, HTML",
+            "React, HTML, C++",
+            "Wordpress, Servers, Blabla",
+            "HTML, CSS, Javascript"
+        ]
+            }
+            {
+                num: 2,
+                question: "Bla de Bla Bla bla",
+                answer: "Answer 1",
+                options: [
+                    "Answer 1",
+                    "Answer 2",
+                    "Answer 3",
+                    "Answer 4",
+                    
+                ]
+            }
+
+
+]
