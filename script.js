5b960cfa4d736cf43880b07d371503d895c5a485
Pirooz Wallace <PiroozWallace@outlook.com>
2020-10-21 16:09:30 -0700

    quicksave
    save before computer crashes again

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 questionList = [ 
     {
         num: 1,
-        question: "What are the three major components of website building",
+        qTitle: "What are the three major components of website building",
         answer: "HTML, CSS. Javascript",
         options: [
             "PHP, Java, HTML",
@@ -10,10 +10,11 @@ questionList = [
             "Wordpress, Servers, Blabla",
             "HTML, CSS, Javascript"
         ]
-            }
+            },
             {
+            
                 num: 2,
-                question: "Bla de Bla Bla bla",
+                qTitle: "Bla de Bla Bla bla",
                 answer: "Answer 1",
                 options: [
                     "Answer 1",
@@ -22,13 +23,27 @@ questionList = [
                     "Answer 4",
 
                 ]
+            },
+            {
+                num: 3,
+                qTitle: "What are the blahs of bla",
+                answer: "Answer 3",
+                options: [
+                    "Answer 1",
+                    "Answer 2",
+                    "Answer 3",
+                    "Answer 4"
+                ]
             }
 
 ]
 
+
 var score = 0;
 var initialsList = ["PW", "ACE"]
 var timerDisplay = 0;
+var startBtn = document.querySelector("#start-test")
+var mainEl = document.querySelector("#details");
 
 
 function init()
@@ -42,24 +57,68 @@ function init()
 function startTest(){
 
     init()
+    setUpQuestions()
 
-    setInterval(timer(), 1000)
+    setInterval(timer, 1000)
     
 }
 
 function timer(){
 
+    // lower by one every second
     timerDisplay--
-    var timeShow = document.querySelector("time-left")
-    timeShow.textContent = timerDisplay
-    if(timerDisplay <= 0)
+
+    // 
+    var timeShow = document.querySelector("#time-number")
+   
+    // update timer display
+    timeShow.innerHTML = timerDisplay 
+
+    console.log(timeShow.innerHTML)
+   
+   /* if(timerDisplay <= 0)
     {
         clearInterval()
         return
     }
+    */
+}
+
+function setUpQuestions(var currentQ)
+{
+
+    // set the question title
+    questionTitle = document.createElement("h1")
+    questionTitle.setAttribute("Question", questionList[currentQ].qTitle)
+    questionTitle.textContent = questionList[currentQ].qTitle
+    
+
+    // Render a new li for each todo
+  for (var i = 0; i < questionList[currentQ].options.length; i++) 
+  {
+      var listQ = document.createElement("li")
+      listQ.setAttribute("choice-value", questionList[currentQ].question)
+
+      
+
+   
+   /*
+    var todo = todos[i];
+
+    var li = document.createElement("li");
+    li.textContent = todo;
+    li.setAttribute("data-index", i);
+
+    var button = document.createElement("button");
+    button.textContent = "Complete";
+
+    li.appendChild(button);
+    todoList.appendChild(li);
+    */
 
 }
 
-document.getElementById("button").addEventListener("click", startTest);
+
+startBtn.addEventListener("click", startTest);
 
 
